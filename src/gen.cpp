76c2c63fc89ed138a1c0dac660b2d10b3b607c79
Connor B. <connorbieszk923@gmail.com>
2024-11-28 12:00:02 -0600

    Fixes same password being generated withen the same second

--- src/gen.cpp
+++ src/gen.cpp
@@ -2,7 +2,8 @@
 
 const std::string allowedCharacters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890!.$#&([])";
 
-static std::mt19937 rng(static_cast<unsigned int>(std::time(0)));
+static std::random_device rd;
+static std::mt19937 rng(rd());
 static std::uniform_int_distribution<size_t> dist(0, allowedCharacters.size() - 1);
 
 char randomChar() {
